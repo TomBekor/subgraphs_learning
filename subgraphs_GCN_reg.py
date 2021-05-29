8e93566a1d59755d875fb0eea867d4b3e37da9cb
Tom Bekor <tombekor@Toms-MacBook-Pro.local>
2021-05-29 17:34:29 +0300

    loggers

--- subgraphs_GCN_reg.py
+++ subgraphs_GCN_reg.py
@@ -12,9 +12,9 @@ import numpy as np
 import networkx as nx
 import pickle
 import scipy.sparse as sp
-from miniBatch import Minibatch
+# from miniBatch import Minibatch
 from sklearn.preprocessing import StandardScaler
-import my_LOL
+# import my_LOL
 import LOL.lol_graph as lol
 
 '''
@@ -27,9 +27,8 @@ CHOOSE OPTIONS IN MAIN
 '''
 
 
-
 class GCN_subgraphs:
-    def __init__(self,GS=True, nni=False):
+    def __init__(self, GS=True, nni=False):
         self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if GS:
             self.load_data_GraphSaint()
@@ -37,7 +36,7 @@ class GCN_subgraphs:
             self._load_data_small_graphs()
         self._nni = nni
 
-    #load the graphSaint datasets
+    # load the graphSaint datasets
     def load_data_GraphSaint(self):
         temp_data = self.load_m()
         train_data = self.process_graph_data(*temp_data)
@@ -48,21 +47,20 @@ class GCN_subgraphs:
         self._num_classes = class_arr.shape[1]
         # adj = _coo_scipy2torch(adj_full_norm.tocoo())
         print("create graph")
-        t=time.time()
-        graph=nx.from_scipy_sparse_matrix(adj_full)
-        #convert the graph to the LOL format
+        t = time.time()
+        graph = nx.from_scipy_sparse_matrix(adj_full)
+        # convert the graph to the LOL format
         undirected_graph = lol.LolGraph(directed=False, weighted=False)
         undirected_graph.convert(list(graph.edges))
         self._g = undirected_graph
-        print("took", time.time()-t)
+        print("took", time.time() - t)
         # nx.write_edgelist(self._g, "amazon.edgelist")
         # self._labels = torch.tensor(np.argwhere(class_arr==1).T[1])
         self._labels = torch.tensor(class_arr)
-        self._X = torch.tensor(feat_full).to(dtype=torch.float )
+        self._X = torch.tensor(feat_full).to(dtype=torch.float)
         self.in_features = feat_full.shape[1]
 
-
-    #relevant for the samll datasets
+    # relevant for the samll datasets
     def _load_data_small_graphs(self):
         # m here complete, fill the model runner from reg notmnt, and configurations and loss
         data_transform = None
@@ -85,11 +83,10 @@ class GCN_subgraphs:
             self._X = self._data.x
             self.in_features = self._data.num_features
 
-        else: #2k vectors input
+        else:  # 2k vectors input
             self._X = None
             self.in_features = num_classes * 2
             self._num_classes = num_classes
-
 
     def create_graph(self):
         nodes = list(range(self._data.num_nodes))
@@ -127,7 +124,7 @@ class GCN_subgraphs:
         # -------------------------
         return adj_full, adj_train, feats, class_map, role
 
-    def process_graph_data(self,adj_full, adj_train, feats, class_map, role):
+    def process_graph_data(self, adj_full, adj_train, feats, class_map, role):
         """
         setup vertex property map for output classes, train/val/test masks, and feats
         """
@@ -145,33 +142,32 @@ class GCN_subgraphs:
                 class_arr[k][v - offset] = 1
         return adj_full, adj_train, feats, class_arr, role
 
-
-    def train(self,  GS, BOW, input_params=None):
+    def train(self, GS, BOW, input_params=None):
         if input_params is None:
 
-            beta = 1 /5
-            gamma = 1 / (5*5)
+            beta = 1 / 5
+            gamma = 1 / (5 * 5)
             try:
-                data=self._data
+                data = self._data
             except:
-                data=None
+                data = None
 
             _ = main_gcn(graph=self._g, X=self._X, data=data,
-                         labels=self._labels, in_features = self.in_features,
-                         hid_features=25, out_features= self._num_classes,ds_name = dataSetName ,
-                         epochs=100 ,dropout=0.2923933209489382, lr=0.00448457, l2_pen=0.0029646435597208516,
-                         beta = beta, gamma = gamma, GS=GS, BOW=BOW,
+                         labels=self._labels, in_features=self.in_features,
+                         hid_features=25, out_features=self._num_classes, ds_name=dataSetName,
+                         epochs=100, dropout=0.2923933209489382, lr=0.00448457, l2_pen=0.0029646435597208516,
+                         beta=beta, gamma=gamma, GS=GS, BOW=BOW,
                          trials=1, dumping_name='',
                          optimizer=Adam,
                          is_nni=self._nni)
 
 
-        else: # NNI
+        else:  # NNI
             beta = 1 / avarage_deg
             gamma = 1 / (avarage_deg * avarage_deg)
             _ = main_gcn(adj_matrices=self._adjacency_matrices,
-                         labels=self._labels, in_features = num_classes*2,
-                         hid_features=int(input_params["hid_features"]), out_features= num_classes,ds_name = dataSetName,
+                         labels=self._labels, in_features=num_classes * 2,
+                         hid_features=int(input_params["hid_features"]), out_features=num_classes, ds_name=dataSetName,
                          epochs=input_params["epochs"], dropout=input_params["dropout"],
                          lr=input_params["lr"], l2_pen=input_params["regularization"],
                          beta=beta, gamma=gamma,
@@ -179,6 +175,7 @@ class GCN_subgraphs:
                          optimizer=input_params["optimizer"],
                          is_nni=self._nni)
         return None
+
 
 def _coo_scipy2torch(adj):
     """
@@ -188,7 +185,8 @@ def _coo_scipy2torch(adj):
     indices = np.vstack((adj.row, adj.col))
     i = torch.LongTensor(indices)
     v = torch.FloatTensor(values)
-    return torch.sparse.FloatTensor(i,v, torch.Size(adj.shape))
+    return torch.sparse.FloatTensor(i, v, torch.Size(adj.shape))
+
 
 def adj_norm(adj, deg=None, sort_indices=True):
     """
@@ -206,9 +204,9 @@ def adj_norm(adj, deg=None, sort_indices=True):
     Note that after 'dot' the indices of a node would be in descending order
     rather than ascending order
     """
-    diag_shape = (adj.shape[0],adj.shape[1])
+    diag_shape = (adj.shape[0], adj.shape[1])
     D = adj.sum(1).flatten() if deg is None else deg
-    norm_diag = sp.dia_matrix((1/D,0),shape=diag_shape)
+    norm_diag = sp.dia_matrix((1 / D, 0), shape=diag_shape)
     adj_norm = norm_diag.dot(adj)
     if sort_indices:
         adj_norm.sort_indices()
@@ -217,22 +215,23 @@ def adj_norm(adj, deg=None, sort_indices=True):
 
 if __name__ == "__main__":
 
-    GraphSaint = True #put true if you want to use the GraphSaint model. If so, declare the dataset name in sys.argv[1] to be one of the following: amazon, yelp, ppi, flickr, ogbn-arxiv
-    BOW=None
+    GraphSaint = True  # put true if you want to use the GraphSaint model. If so, declare the dataset name in sys.argv[1] to be one of the following: amazon, yelp, ppi, flickr, ogbn-arxiv
+    BOW = None
 
     # for graphSaint:
     if GraphSaint:
-        dataSetName =sys.argv[1]
+        dataSetName = sys.argv[1]
     else:
-        BOW = False #change to true if GraphSaint is False and also if you want to use the bag of words input (and not the 2k vectors).
+        BOW = False  # change to true if GraphSaint is False and also if you want to use the bag of words input (and not the 2k vectors).
         # choose dataset: relevant when running on the small graphs only (and not on graphsaint):
-        dataSetName = "cora";num_classes = 7; avarage_deg = 3.8980797636632203; directed = True
+        dataSetName = "cora";
+        num_classes = 7;
+        avarage_deg = 3.8980797636632203;
+        directed = True
         # dataSetName = "CiteSeer"; num_classes = 6; avarage_deg  = 2.7363991584009617; directed=True
         # dataSetName = "PubMed"; num_classes = 3; avarage_deg  = 4.496018664096972; directed=False
         # dataSetName = "Physics"; directed=False; avarage_deg  = 4.6
         # dataSetName = "CS"; directed=False; avarage_deg  = 4.46
-
-
 
     gg = GCN_subgraphs(GraphSaint)
     gg.train(GraphSaint, BOW)
