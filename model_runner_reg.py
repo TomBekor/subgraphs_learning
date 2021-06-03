1c2880221d88ec8a88dce42f7acc43e86d9781af
Tom Bekor <tombekor@Toms-MacBook-Pro.local>
2021-06-03 16:44:43 +0300

    1 epoch

--- model_runner_reg.py
+++ model_runner_reg.py
@@ -464,16 +464,18 @@ def execute_runner(runners, plots_data, is_nni=False):
     # for printing results on graphs. for other uses - the last result is the one should be used.
     size = len(results)
     #train_loss_mean = torch.stack([torch.tensor([results[j][4][i] for i in range(len(results[j][4]))]) for j in range(size)]).mean(axis=0)
-    lst = [[results[j][4][i] for i in range(len(results[j][4]))] for j in range(size)]
+    lst = [[results[j][4][i].cpu() for i in range(len(results[j][4]))] for j in range(size)]
     print(type(lst))
     print(type(lst[0]))
     print(type(lst[0][0]))
     print(type(lst[0][0].cpu()))
     train_loss_mean = np.mean(lst, axis=0)
+    print('sususus')
     # # TODO
     # # train_loss_mean = np.mean([[results[j][4][i] for i in range(len(results[j][4]))] for j in range(size)], axis=0)
     #train_acc_mean = torch.stack([ torch.tensor([results[j][5][i] for i in range(len(results[j][5]))]) for j in range(size) ]).mean(axis=0)
     train_acc_mean = np.mean([ [results[j][5][i] for i in range(len(results[j][5]))] for j in range(size) ], axis=0)
+    print('okokok')
     train_cn_acc_mean = np.mean([[results[j][6][i] for i in range(len(results[j][6]))] for j in range(size)], axis=0)
     train_f_acc_mean = np.nanmean([[results[j][7][i] for i in range(len(results[j][7]))] for j in range(size)], axis=0)
     train_s_acc_mean = np.nanmean([[results[j][8][i] for i in range(len(results[j][8]))] for j in range(size)], axis=0)
